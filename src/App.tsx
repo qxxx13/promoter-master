d6562b2d94b6be12955ac472248bfd4536d39ac5
Nikolay Nazarenko <redecv9@gmail.com>
2024-03-16 16:54:40 +0300

    add: initialize telegram web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
-    return <div className="App"></div>;
+    const onClose = () => {
+        Telegram.WebApp.close();
+    };
+
+    useEffect(() => {
+        Telegram.WebApp.ready();
+    }, []);
+
+    return (
+        <div>
+            <button onClick={onClose}>Закрыть</button>
+        </div>
+    );
 }
 
 export default App;
