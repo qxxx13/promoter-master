9e69b6dd100ea8e5fe80bfec4c459be51bba0bdc
Nikolay Nazarenko <redecv9@gmail.com>
2024-03-17 17:00:18 +0300

    test: dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { extendTheme } from '@mui/joy';
+import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppRouter } from './router/AppRouter';
 
 export const App = () => {
+    const theme = extendTheme({ cssVarPrefix: 'demo' });
+
     return (
         <BrowserRouter>
-            <AppRouter />
+            <JoyCssVarsProvider
+                defaultMode="dark"
+                theme={theme}
+                colorSchemeSelector="#demo_dark-mode-by-default"
+                modeStorageKey="demo_dark-mode-by-default"
+                disableNestedContext
+            >
+                <AppRouter />
+            </JoyCssVarsProvider>
         </BrowserRouter>
     );
 };
